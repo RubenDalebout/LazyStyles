baec2030d7ad4c5fe679eccf46a6d45d40225a39
DeEchteZeeuw <rubendalebout@gmail.com>
2023-01-20 01:13:21 +0100

    fixed logos

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,26 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
+  <Header />
   <RouterView />
+
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac interdum ex. Aliquam eu volutpat quam, vitae commodo felis. Nam vulputate condimentum tellus quis ornare. Aenean maximus justo eget risus tempus facilisis. Nam vel fringilla nisl. Aenean quis libero ex. Proin nec justo molestie lacus malesuada elementum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vel enim venenatis turpis dictum suscipit non sed lorem. Pellentesque vel egestas urna, a convallis erat. Ut a augue vehicula, varius nibh non, ultrices lectus. In nec facilisis mauris. Nunc eget luctus est. Suspendisse a augue vestibulum, blandit augue quis, bibendum purus. Fusce pharetra id nunc sed porttitor. Vivamus posuere gravida mauris, et tincidunt ligula auctor sit amet.
+
+Donec a diam est. Suspendisse potenti. Donec rutrum, nisl ut dapibus ornare, purus lorem dictum massa, nec tempor nulla odio nec est. Nullam nec nisi nisi. Donec sit amet ex id sapien mattis tempor eu ac metus. Etiam ac orci in purus posuere hendrerit. Vestibulum quis erat erat. Proin lacinia, turpis eu laoreet vestibulum, ligula justo rutrum purus, mollis dictum elit ipsum ut ipsum. Phasellus interdum maximus lectus eu mollis. Suspendisse potenti. Vivamus finibus congue enim, a laoreet lorem venenatis in.
+
+Morbi dignissim lectus nec neque efficitur, aliquam sodales purus tempor. Nam vel dapibus velit, a mattis nisl. Praesent feugiat lorem quis nisl faucibus, nec volutpat nibh venenatis. Phasellus luctus, nunc et laoreet dapibus, erat turpis tempus arcu, ac porttitor magna mauris eu urna. Etiam interdum, elit sit amet auctor sagittis, urna arcu tempus odio, suscipit semper nisl tellus sit amet nibh. Sed in neque nec nibh aliquam dignissim et ut justo. Cras volutpat, diam sit amet auctor ultricies, turpis urna dignissim turpis, vitae bibendum eros eros at leo. Cras scelerisque pellentesque congue. Curabitur euismod libero scelerisque consequat egestas. Sed tortor felis, ultricies vitae fringilla ut, ornare id tortor. Aliquam a nunc vitae massa cursus ultrices sit amet tempor nulla. Nam placerat sapien nec ipsum mollis, sed tincidunt elit consectetur. Nullam venenatis nulla erat. Nulla facilisi.
+
+Mauris sed felis eu augue mollis eleifend. Ut feugiat lorem ac commodo aliquet. Duis quis sapien quis quam malesuada porttitor. Suspendisse sollicitudin diam risus, et dapibus risus tempor ac. Donec molestie dolor enim, a molestie magna dictum ac. Nunc ante ex, finibus at viverra sed, ullamcorper vel mi. Maecenas ac lacus vitae metus dignissim elementum. Pellentesque varius velit turpis, in pretium enim sodales eu. Nunc faucibus ultrices quam, vitae facilisis dui porta vel. Maecenas rutrum semper neque, ac varius sapien tempus id. Nunc volutpat urna sit amet condimentum tristique. In gravida ipsum eget orci auctor aliquet. Nam mauris lectus, congue eget faucibus at, posuere quis sem. Proin maximus libero at mi dignissim rutrum.
+
+Mauris quis porta magna, vitae bibendum magna. Ut fermentum arcu blandit quam sollicitudin, sit amet malesuada est pellentesque. Phasellus id turpis nisi. Fusce sit amet augue in tortor malesuada iaculis. Sed vel augue lorem. Donec vel urna eget metus cursus lacinia. Duis nec lectus purus. Vestibulum at tincidunt ligula. Cras id tincidunt risus, egestas scelerisque orci. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Phasellus in convallis nibh. Donec finibus tempor viverra. Vivamus vel sagittis massa. Integer maximus arcu id mauris facilisis varius.
 </template>
+
+<script>
+import Header from '@/components/templates/header.vue'
+
+export default {
+  components: {
+    Header
+  }
+}
+</script>
---
+++ src/components/templates/header.vue
@@ -0,0 +1,34 @@
+<template>
+    <header class="sticky-top">
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">
+                    <img v-bind:src="this.logo" width="50" alt="LazyStyles logo">
+                    Pure Generation
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarToggler">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <router-link to="/" active-class="active">Home</router-link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+</template>
+
+<script>
+    import logoLight from '@/assets/img/logos/android-chrome-192x192.png'
+
+    export default {
+        data() {
+            return {
+                logo: logoLight
+            }
+        }
+    }
+</script>
