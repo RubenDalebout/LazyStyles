f1d2dc34cbcd8d9c0dd9f24506335f439ab0495e
DeEchteZeeuw <rubendalebout@gmail.com>
2023-01-25 19:37:49 +0100

    Done all styling

--- src/App.vue
+++ src/App.vue
@@ -5,27 +5,6 @@ import { RouterView } from 'vue-router'
 <template>
   <Header />
   <RouterView />
-
-  <button type="button" class="btn btn-primary">Primary</button>
-  <button type="button" class="btn btn-secondary">Secondary</button>
-
-  <div class="alert alert-primary" role="alert">
-    A simple primary alert—check it out!
-  </div>
-  <div class="alert alert-secondary" role="alert">
-    A simple secondary alert—check it out!
-  </div>
-
-  <span class="badge text-bg-primary">Primary</span>
-  <span class="badge text-bg-secondary">Secondary</span>
-  
-  <ul class="list-group">
-  <li class="list-group-item active" aria-current="true">An active item</li>
-  <li class="list-group-item">A second item</li>
-  <li class="list-group-item">A third item</li>
-  <li class="list-group-item">A fourth item</li>
-  <li class="list-group-item">And a fifth one</li>
-</ul>
 </template>
 
 <script>
