f378c2998f5fe4a09abe13019988490540373a8a
DeEchteZeeuw <rubendalebout@gmail.com>
2023-01-25 19:35:44 +0100

    Changed active list-group color

--- src/App.vue
+++ src/App.vue
@@ -18,6 +18,14 @@ import { RouterView } from 'vue-router'
 
   <span class="badge text-bg-primary">Primary</span>
   <span class="badge text-bg-secondary">Secondary</span>
+  
+  <ul class="list-group">
+  <li class="list-group-item active" aria-current="true">An active item</li>
+  <li class="list-group-item">A second item</li>
+  <li class="list-group-item">A third item</li>
+  <li class="list-group-item">A fourth item</li>
+  <li class="list-group-item">And a fifth one</li>
+</ul>
 </template>
 
 <script>
